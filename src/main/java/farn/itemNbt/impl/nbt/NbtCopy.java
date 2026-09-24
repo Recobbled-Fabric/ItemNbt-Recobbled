01832157ef5e4b8aa82c0f52ef8635a99e58a9c9
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-09-24 07:56:40 +0700

    add back NbtBase Copy

--- src/main/java/farn/itemNbt/impl/nbt/NbtCopy.java
+++ src/main/java/farn/itemNbt/impl/nbt/NbtCopy.java
@@ -3,5 +3,7 @@ package farn.itemNbt.impl.nbt;
 import net.minecraft.src.NBTBase;
 
 public interface NbtCopy {
-    NBTBase copy();
+    default NBTBase copy() {
+        return (NBTBase)this;
+    }
 }
